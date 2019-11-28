004d37e634f30a3784be3e8f1372813b4f626bba
Marcos Cezar <cezar.mp23@gmail.com>
2019-11-28 01:30:10 -0300

    alterando projetor

--- js/Classes/Projetor.js
+++ js/Classes/Projetor.js
@@ -1,14 +1,26 @@
-class Projetor{
-    constructor(ctx, listaDeObjetos, matrizDeProjecao){
+class Projetor {
+    constructor(ctx, listaDeObjetos, matrizDeProjecao) {
         this.ctx = ctx;
         this.listaDeObjetos = listaDeObjetos;
         this.matrizDeProjecao = matrizDeProjecao;
     }
 
-    projetar(){
+    projetar() {
         this.listaDeObjetos.forEach(element => {
             this.desenhar_linhas(element);
         });
+    }
+    projetarFinal() {
+        this.listaDeObjetos.forEach(element => {
+            this.desenhaFinal(element);
+        });
+    }
+    produtoInterno(v1, v2) {
+        var total = 0;
+        for (var i = 0; i < v1.length; i++) {
+            total += v1[i] * v2[i];
+        }
+        return total;
     }
 
     desenhar_linhas(objeto) {
@@ -16,7 +28,7 @@ class Projetor{
         this.ctx.beginPath()
 
         //For para desenhar todas as arestas
-        for(var i = 0; i<objeto.arestas.length; i++) {
+        for (var i = 0; i < objeto.arestas.length; i++) {
             //Posiciona o cursor nas coordenadas definidas pelo primeiro vertice da aresta
             this.ctx.moveTo(objeto.a1[objeto.arestas[i][0]][0], objeto.a1[objeto.arestas[i][0]][1])
             //Desenha uma linha até o segundo vértice da aresta
@@ -28,5 +40,48 @@ class Projetor{
         this.ctx.closePath()
 
     }
+
+    desenhaFinal(objeto) {
+        this.ctx.beginPath()
+        var normal = objeto.normal_plano()
+        var arr = [];
+        for (var j = 0; j < objeto.faces.length; j++) {
+            var t = [objeto.coordenadas[objeto.arestas[objeto.faces[j][0]][0]][0] - 0, objeto.coordenadas[objeto.arestas[objeto.faces[j][0]][0]][1] - 0, objeto.coordenadas[objeto.arestas[objeto.faces[j][0]][0]][2] - 250];
+            console.log(objeto.faces[j])
+            console.log(t);
+            console.log(this.produtoInterno(t, normal[j]));
+
+            if (this.produtoInterno(t, normal[j]) >= 0) {
+                for (var i = 0; i < objeto.faces[j].length; i++) {
+                    arr.push(objeto.arestas[objeto.faces[j][i]]);
+                }
+            }
+        }
+        console.log(arr);
+
+        for (var i = 0; i < arr.length; i++) {
+            //Posiciona o cursor nas coordenadas definidas pelo primeiro vertice da aresta
+            this.ctx.moveTo(objeto.a1[arr[i][0]][0], objeto.a1[arr[i][0]][1])
+            //Desenha uma linha até o segundo vértice da aresta
+            this.ctx.lineTo(objeto.a1[arr[i][1]][0], objeto.a1[arr[i][1]][1])
+        }
+
+        //Método que desenha tudo que foi "demarcado" na tela
+        this.ctx.stroke()
+        this.ctx.closePath()
+
+        }
+        /*
+        for(var i = 0; i<objeto.arestas.length; i++) {
+            //Posiciona o cursor nas coordenadas definidas pelo primeiro vertice da aresta
+            this.ctx.moveTo(objeto.a1[objeto.arestas[i][0]][0], objeto.a1[objeto.arestas[i][0]][1])
+            //Desenha uma linha até o segundo vértice da aresta
+            this.ctx.lineTo(objeto.a1[objeto.arestas[i][1]][0], objeto.a1[objeto.arestas[i][1]][1])
+        }*/
+
+        //Método que desenha tudo que foi "demarcado" na tela
+        //this.ctx.stroke()
+        //this.ctx.closePath()
+    
 
 }
